2f5dcba3e4e7ad7d06396fa39a20d0b9e180dc00
mariyamango <de.msomkova@gmail.com>
2024-09-02 11:25:03 +0200

    added new tests and method

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -5,9 +5,39 @@ import java.util.Arrays;
 public class PlayerCharacter {
     public static void main(String[] args) {
         System.out.println(getX());
+        System.out.println(getY());
+        int x = 0, y = 0;
+        System.out.println(Arrays.toString(move('W', new int[]{x, y})));
+        System.out.println(Arrays.toString(move('S', new int[]{x, y})));
+        System.out.println(Arrays.toString(move('A', new int[]{x, y})));
+        System.out.println(Arrays.toString(move('D', new int[]{x, y})));
     }
 
     public static int getX() {
         return 0;
+    }
+
+    public static int getY(){
+        return 0;
+    }
+
+    public static int[] move (char direction, int[] coord){
+        //step up
+        if (direction == 'W') {
+            coord[1] = coord[1] + 1;
+        }
+        //step down
+        else if (direction == 'S') {
+            coord[1] = coord[1] - 1;
+        }
+        //step left
+        else if (direction == 'A') {
+            coord[0] = coord[0] - 1;
+        }
+        //step right
+        else if (direction == 'D') {
+            coord[0] = coord[0] + 1;
+        }
+        return coord;
     }
 }
--- src/test/java/org/example/PlayerCharacterTest.java
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerCharacterTest {
+
     @Test
     void getX_expect0(){
         //Given
-        int x = 0;
         int expected = 0;
         //When
         int actual = PlayerCharacter.getX();
@@ -15,6 +15,57 @@ public class PlayerCharacterTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    void getY_expect0(){
+        //Given
+        int expected = 0;
+        //When
+        int actual = PlayerCharacter.getY();
+        //Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void move_whenWExpectYIs1(){
+        //Given
+        char direction = 'W';
+        //When
+        int[] actualCoords = PlayerCharacter.move(direction,new int[]{0,0});
+        //Then
+        Assertions.assertEquals(actualCoords[0], 0);
+        Assertions.assertEquals(actualCoords[1], 1);
+    }
+
+    @Test
+    void move_whenSExpectYIsMinus1(){
+        //Given
+        char direction = 'S';
+        //When
+        int[] actualCoords = PlayerCharacter.move(direction,new int[]{0,0});
+        //Then
+        Assertions.assertEquals(actualCoords[0], 0);
+        Assertions.assertEquals(actualCoords[1], -1);
+    }
+    @Test
+    void move_whenDExpectXIs1(){
+        //Given
+        char direction = 'D';
+        //When
+        int[] actualCoords = PlayerCharacter.move(direction,new int[]{0,0});
+        //Then
+        Assertions.assertEquals(actualCoords[0], 1);
+        Assertions.assertEquals(actualCoords[1], 0);
+    }
+    @Test
+    void move_whenAExpectXIsMinus1(){
+        //Given
+        char direction = 'A';
+        //When
+        int[] actualCoords = PlayerCharacter.move(direction,new int[]{0,0});
+        //Then
+        Assertions.assertEquals(actualCoords[0], -1);
+        Assertions.assertEquals(actualCoords[1], 0);
+    }
 }
 
 
